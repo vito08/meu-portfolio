6c8abdb86467034b1deb659090f549c692b19258
Virtu__ <144934043+vito08@users.noreply.github.com>
2025-03-13 16:34:44 -0300

    updating script.js

--- Portfólio/script.js
+++ Portfólio/script.js
@@ -10,3 +10,30 @@ function changeProfileImage() {
         name.textContent = "Victor Hugo";
     }
 }
+
+function infoChange(section) {
+    var description = document.getElementById("description-content");
+    var hardSkill = document.getElementById("hard-skills-content");
+    var softSkill = document.getElementById("soft-skills-content");
+
+    var descriptionButton = document.getElementById("description");
+    var hardSkillButton = document.getElementById("hard-skills");
+    var softSkillButton = document.getElementById("soft-skills");
+
+    description.style.display = "none";
+    hardSkill.style.display = "none";
+    softSkill.style.display = "none";
+
+    if (section === "description") {
+        description.style.display = "block";
+        descriptionButton.style.order = "1"
+        hardSkillButton.style.order = "0"
+    } else if (section === "hardSkill") {
+        hardSkill.style.display = "flex";
+        hardSkillButton.style.order = "1"
+    } else if (section === "softSkill") {
+        softSkill.style.display = "flex";
+        softSkillButton.style.order = "1"
+        hardSkillButton.style.order = "2"
+    }
+}
